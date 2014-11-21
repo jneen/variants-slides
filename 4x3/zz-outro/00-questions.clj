3739fcc2a4cbc46c238c363a3bc07bd8c68ae9e4
http://jneen.net/ <jneen@jneen.net>
2014-11-21 07:57:48 -0800

    reformat zz-outro for 4x3

---
+++ 4x3/zz-outro/00-questions.clj
@@ -0,0 +1,18 @@
+
+  [:clojure/conj 2014 "Washington, DC"]
+
+
+
+
+
+              Variants are Not Unions
+
+            (-._.)? [QUESTIONS] l(O.e7)
+
+
+
+                   { :name "Jeanine Adkisson"
+                     :twitter "@jneen_"
+                     :github "@jneen"
+                     :blog "http://jneen.net/" }
+
